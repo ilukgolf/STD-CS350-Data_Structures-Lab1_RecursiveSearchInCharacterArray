6018bb774c0678591b990a83f4589c5cbf03d51e
ilukgolf <74899757+ilukgolf@users.noreply.github.com>
2025-09-09 16:09:52 +0700

    Create Lab1-RecursiveSearchInCharacterArray-1630708046.py

---
+++ Lab1-RecursiveSearchInCharacterArray-1630708046.py
@@ -0,0 +1,20 @@
+# Lab 1: Recursive Search In Character Array
+# Student: Jirawat Bunmaraksasakul
+# ID: 1630708046
+
+# Data set to search from
+# Note: Some strings intentionally contain lowercase characters (e.g., 'q', 'm', 'u', 'h') for testing recursive search functions.
+data = [
+    "QWERTYUIOPASDFGHJKLZXCVBNM",
+    "ASDFGHJKLZXCVBNMqWERTYUIOP",
+    "ZXCVBNMASDFGHJKLZWERTYUIOP",
+    "POIUYTREWQLKJHGFDSAmNBVCXZ",
+    "MNBVCXZLKJHGFDSAPOIUYTREWQ",
+    "LKJHGFDSAZXCVBNMqPOIUYTREW",
+    "ASDFGHJKLZXCVBNMWERTYuIOP",
+    "QWERTYUIOPASDFGHJKLZXCVBNM",
+    "ZXCVBNMASDFGHJKLZWERTYUIOP",
+    "POIUYTREWQLKJHGFDSAZXCVBNM",
+    "MNBVCXZLKJhGFDSAPOIUYTREWQ",
+    "QWERTYUIOPASDFGHJKLZXCVBNM"
+]
